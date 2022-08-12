f3b9100998caa58793d6ca055a52a7c9e0acb3f6
Anthony Pachco <ajp6548@psu.edu>
2022-08-12 15:38:58 -0400

    got page to switch correctly

--- View/main.js
+++
@@ -1,8 +0,0 @@
-const login = document.getElementsByClassName("login"); 
-const newAccount = document.getElementById("newAccount");
-function createAccount() {
-    //login.style.display.toggle('none');
-   // newAccount.style.display = 'block';
-   window.location.replace("createAccount.html");
-    console.log("THIS CLICKS");
-}
