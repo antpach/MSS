c0376dd906d881c3fb034a3ba11e743e4e97a36c
Anthony Pachco <ajp6548@psu.edu>
2022-08-12 15:43:23 -0400

    add main correctly

---
+++ main.js
@@ -0,0 +1,8 @@
+const login = document.getElementById("login"); 
+const newAccount = document.getElementById("newAccount");
+
+ document.getElementById("create").onclick =() =>{
+     login.style.display='none';
+     newAccount.style.display = 'block';
+}
+
