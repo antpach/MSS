13d8422e019478249e567ad6ebd20816484cef06
Anthony Pachco <ajp6548@psu.edu>
2022-08-13 19:59:57 -0400

    added Account controller file with create method

---
+++ Controller/AccountController.js
@@ -0,0 +1,40 @@
+//import { DB_Controller_Class} from './DB_Controller.js';
+// import { Client } from'../Model/User.js';
+const DB_Controller = require('./DB_Controller.js')
+const {Client, User} = require('../User.js')
+
+class AccountController{
+     create(client) {
+        DB_Controller_Class
+            .addClient(new Client(client.name, client.address,client.username, client.password))
+            .then((response) => {
+                console.log(`user account created\n${client.username} signed in`);
+    
+                return true; 
+            
+    
+            }).catch((error) => {
+                let message = null;
+                switch (error) {
+                    case Response.USERNAME_TAKEN:
+                        message = 'User name taken';
+                        console.log(message);
+                        break;
+                    case Response.INVALID_ARGUMENT:
+                        throw new Error('Invalid User Model provided');
+                    default:
+                        message = 'Unknown error when logging in. Please try again.'
+                        console.log(message);
+                }
+                console.log('sending error message...');
+                return false; 
+            });
+        }
+}
+   
+
+    module.exports = {
+        AccountController
+   
+    }
+
